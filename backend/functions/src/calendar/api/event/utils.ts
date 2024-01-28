555d59b35930b2accfe5a947bb6cf17f129a543a
GuillaumeSD <gsd.lfny@gmail.com>
2024-01-28 20:27:34 +0100

    feat : add event reminders

--- backend/functions/src/calendar/api/event/utils.ts
+++ backend/functions/src/calendar/api/event/utils.ts
@@ -1,5 +1,5 @@
 import { GameDto } from "../../../dto/competition";
-import { CalendarEventParams } from "../../../types/calendar";
+import { CalendarEvent, CalendarEventParams } from "../../../types/calendar";
 import { addXHours } from "../../../utils/helpers";
 
 export const gameToEventParams = (
@@ -37,9 +37,66 @@ export const gameToEventParams = (
           },
         ]
       : undefined,
+    reminders: {
+      useDefault: false,
+      overrides: [{ method: "popup", minutes: 90 }],
+    },
   };
 };
 
 export const getEventIdFromGame = (game: GameDto): string => {
   return game.ffvbId.toLowerCase();
+};
+
+export const isEventAlreadyUpdated = (
+  event: CalendarEvent | undefined,
+  game: GameDto,
+  eventParams: CalendarEventParams
+) => {
+  if (!event) return false;
+
+  const areDatesEqual =
+    eventParams?.start?.dateTime?.split("T")[0] ===
+      event.start?.dateTime?.split("T")[0] &&
+    game.time === event.start?.dateTime?.split("T")[1].slice(0, 5) &&
+    eventParams?.end?.dateTime?.split("T")[0] ===
+      event.end?.dateTime?.split("T")[0];
+
+  const isSpecialNoTimeGame =
+    !game.time &&
+    eventParams?.end?.dateTime?.split("T")[0] ===
+      event.end?.dateTime?.split("T")[0];
+
+  const areAttachmentsEqual =
+    eventParams?.attachments?.length === event.attachments?.length &&
+    eventParams?.attachments?.every((attachment) =>
+      event.attachments?.some(
+        (eventAttachment) =>
+          eventAttachment.fileUrl === attachment.fileUrl &&
+          eventAttachment.title === attachment.title &&
+          eventAttachment.mimeType === attachment.mimeType &&
+          eventAttachment.iconLink === attachment.iconLink
+      )
+    ) !== false;
+
+  const areRemindersEqual =
+    eventParams?.reminders?.useDefault === event.reminders?.useDefault &&
+    eventParams?.reminders?.overrides?.length ===
+      event.reminders?.overrides?.length &&
+    eventParams?.reminders?.overrides?.every((reminder) =>
+      event.reminders?.overrides?.some(
+        (eventReminder) =>
+          eventReminder.method === reminder.method &&
+          eventReminder.minutes === reminder.minutes
+      )
+    ) !== false;
+
+  return (
+    eventParams?.summary === event.summary &&
+    eventParams?.description === event.description &&
+    eventParams?.location === event.location &&
+    (areDatesEqual || isSpecialNoTimeGame) &&
+    areAttachmentsEqual &&
+    areRemindersEqual
+  );
 };
--- backend/functions/src/calendar/event.ts
+++ backend/functions/src/calendar/event.ts
@@ -1,9 +1,13 @@
 import { CompetitionMetadataDto, GameDto } from "../dto/competition";
-import { CalendarEvent, CalendarEventParams } from "../types/calendar";
+import { CalendarEvent } from "../types/calendar";
 import { createCalendarEvent } from "./api/event/create";
 import { deleteCalendarEvent } from "./api/event/delete";
 import { updateCalendarEvent } from "./api/event/update";
-import { gameToEventParams, getEventIdFromGame } from "./api/event/utils";
+import {
+  gameToEventParams,
+  getEventIdFromGame,
+  isEventAlreadyUpdated,
+} from "./api/event/utils";
 
 export const getSortedGames = (games: GameDto[], calendar: CalendarEvent[]) => {
   const gamesToCreateEvents = games.filter(
@@ -57,46 +61,6 @@ export const handleUpdateEvent =
 
     await updateCalendarEvent(eventId, calendarId, eventParams);
   };
-
-const isEventAlreadyUpdated = (
-  event: CalendarEvent | undefined,
-  game: GameDto,
-  eventParams: CalendarEventParams
-) => {
-  if (!event) return false;
-
-  const areDatesEqual =
-    eventParams?.start?.dateTime?.split("T")[0] ===
-      event.start?.dateTime?.split("T")[0] &&
-    game.time === event.start?.dateTime?.split("T")[1].slice(0, 5) &&
-    eventParams?.end?.dateTime?.split("T")[0] ===
-      event.end?.dateTime?.split("T")[0];
-
-  const isSpecialNoTimeGame =
-    !game.time &&
-    eventParams?.end?.dateTime?.split("T")[0] ===
-      event.end?.dateTime?.split("T")[0];
-
-  const areAttachmentsEqual =
-    eventParams?.attachments?.length === event.attachments?.length &&
-    eventParams?.attachments?.every((attachment) =>
-      event.attachments?.some(
-        (eventAttachment) =>
-          eventAttachment.fileUrl === attachment.fileUrl &&
-          eventAttachment.title === attachment.title &&
-          eventAttachment.mimeType === attachment.mimeType &&
-          eventAttachment.iconLink === attachment.iconLink
-      )
-    ) !== false;
-
-  return (
-    eventParams?.summary === event.summary &&
-    eventParams?.description === event.description &&
-    eventParams?.location === event.location &&
-    (areDatesEqual || isSpecialNoTimeGame) &&
-    areAttachmentsEqual
-  );
-};
 
 export const handleDeleteEvent =
   (calendarId: string) => async (event: CalendarEvent) => {
